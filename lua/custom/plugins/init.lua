3cff17f782417af7b72bfd49cafa095e4fa6b675
ergolyam <grisha765@tuta.io>
2025-11-21 03:19:08 +0300

    [+] install & conf langmapper

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,4 +30,24 @@ return {
       end, { desc = 'Telescope File Browser' })
     end,
   },
+  {
+    'Wansmer/langmapper.nvim',
+    lazy = false,
+    priority = 1,
+    config = function()
+      require('langmapper').setup { global = true, buffer = true }
+      local function escape(str)
+        local escape_chars = [[;,."|\]]
+        return vim.fn.escape(str, escape_chars)
+      end
+      local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+      local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+      local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+      local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+      vim.opt.langmap = vim.fn.join({
+        escape(ru_shift) .. ';' .. escape(en_shift),
+        escape(ru) .. ';' .. escape(en),
+      }, ',')
+    end,
+  },
 }
