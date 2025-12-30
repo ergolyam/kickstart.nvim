d20e6c197d355b162d58f135a0205d9558137928
ergolyam <grisha765@tuta.io>
2025-12-30 20:14:00 +0300

    [r] use pyright

--- init.lua
+++ init.lua
@@ -711,8 +711,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        ty = {},
+        pyright = {},
+        -- ty = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
